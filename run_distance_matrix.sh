c5b647af4cdbc08269a8eb16f2ba7283a423fa08
semccomas <semccomas@gmail.com>
2018-10-16 15:20:12 +0200

    updating to intxn dir

--- run_distance_matrix.sh
+++
@@ -1,8 +0,0 @@
-rep=1
-traj=/data2/segmented_ATP_synthase/replica_$rep/production/ATPs$rep.0-4us.skip10.xtc
-
-python distance_matrix.py AN_replica_$rep/ATPs$rep.0ns.chainref.pdb $traj POPC dimer_1 
-python distance_matrix.py AN_replica_$rep/ATPs$rep.0ns.chainref.pdb $traj POPC dimer_2
-
-
-
